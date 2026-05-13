b1568e1f4a2666d0905987d466dcef3334219263
sombath.Eat <sbath5261@gmail.com>
2026-05-13 21:34:50 +0700

    update on project page

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,38 @@
-import { Col } from "react-bootstrap";
+// import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+// export const ProjectCard = ({ title, description, imgUrl }) => {
+//   return (
+//     <Col size={12} sm={6} md={4}>
+//       <div className="proj-imgbx">
+//         <img src={imgUrl} alt={title} />
+//         <div className="proj-txtx">
+//           <h4>{title}</h4>
+//           <span>{description}</span>
+//         </div>
+//       </div>
+//     </Col>
+//   )
+// }
+
+import { Col } from 'react-bootstrap';
+
+export const ProjectCard = ({ title, description, imgUrl, link }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} alt={title} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+      <a
+        href={link || '#'}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: 'none', color: 'white' }}
+      >
+        <div className="proj-imgbx">
+          <img src={imgUrl} alt={title} />
+          <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
+          </div>
         </div>
-      </div>
+      </a>
     </Col>
-  )
-}
+  );
+};
